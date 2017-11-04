f9767d445eaf6237443e55d43c59b9544d07af28
tredwood89 <tredwood89@gmail.com>
2017-11-04 01:40:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay (musicians, instruments){
+
+  var beatlesArray=[]
+
+  for (var i = 0, i < 4, i++){
+
+    beatlesArray.push (`${musicians} plays ${instruments}`)
+  }
+      return beatlesArray
+}
